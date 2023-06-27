5950b786f312b4a41d1487122fb57c584859920f
PrefectSol <prefectsol@mail.ru>
2023-06-27 14:59:30 +0500

    Add theme switcher

--- MPM/demo/src/main/java/com/mpm/demo/HelloController.java
+++ MPM/demo/src/main/java/com/mpm/demo/HelloController.java
@@ -5,10 +5,15 @@ import javafx.scene.control.Label;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 public class HelloController {
+    private Boolean blackTheme = false;
+
+
     @FXML
     protected void onFunctionClick() {
         try {
@@ -27,13 +32,45 @@ public class HelloController {
     }
 
     @FXML
+    protected void onSwitchTheme()
+    {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+
+            Scene scene = new Scene(loader.load());
+            Stage stage = new Stage();
+
+            String style;
+
+            if (blackTheme)
+            {
+                style = "-fx-background-color: #696969;";
+            }
+            else
+            {
+                style = "-fx-background-color: #FFFFFF;";
+            }
+
+            blackTheme = !blackTheme;
+
+            scene.getRoot().setStyle("-fx-background-color: #696969;");
+
+            stage.setTitle("IntegralMatcher");
+            stage.setScene(scene);
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
     protected void onIntegralClick()
     {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("IntegralMatcher.fxml"));
 
             Scene scene = new Scene(loader.load());
-
             Stage stage = new Stage();
 
             stage.setTitle("IntegralMatcher");
