c35b70840f824a015eaf7fcbb977a8e1a8c7cb66
student-csu <hnthntjkn@yandex.ru>
2023-06-27 13:58:27 +0500

    фыв2

--- MFI2/src/main/java/com/example/mfi2/HelloApplication.java
+++ MFI2/src/main/java/com/example/mfi2/HelloApplication.java
@@ -18,6 +18,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("sarerqeeeee43");
+        System.out.println("");
     }
 }
