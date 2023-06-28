36807714b84327c3c4d0bdf1559b5ee8e9887d16
Bulbuh <melnikov0109@mail.ru>
2023-06-28 22:12:23 +0500

    ФИО

--- MFI2/src/main/java/com/example/mfi2/Add.java
+++ MFI2/src/main/java/com/example/mfi2/Add.java
@@ -1,4 +1,19 @@
 package com.example.mfi2;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Add {
+    @FXML
+    private Button otmena;
+    @FXML
+    protected void clickAdd(){
+        Stage stage = (Stage) otmena.getScene().getWindow();
+        stage.close();
+    }
 }
