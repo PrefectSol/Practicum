52fb69ba12a82ef67d156face9ddb7e2e652e611
student-csu <student-csu@student-host-12>
2023-06-29 13:18:13 +0500

    del

--- MFI2/src/main/java/com/example/mfi2/HelloController.java
+++ MFI2/src/main/java/com/example/mfi2/HelloController.java
@@ -2,10 +2,13 @@ package com.example.mfi2;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
     private String type = "";
@@ -22,14 +25,18 @@ public class HelloController {
     @FXML
     private TextField value2;
     @FXML
+    private GridPane gp;
+    @FXML
+    private AnchorPane edit;
+    @FXML
     protected void clickAdd(){
-        table.setVisible(false);
+        unVisible();
         addForm.setVisible(true);
     }
     @FXML
     public void otmena() {
+        unVisible();
         table.setVisible(true);
-        addForm.setVisible(false);
     }
     @FXML
     public void switchTheme() {
@@ -42,6 +49,11 @@ public class HelloController {
         sPane.setStyle("-fx-background-color:" + type);
         table.setStyle("-fx-background-color:" + type);
     }
+    protected void unVisible(){
+        table.setVisible(false);
+        addForm.setVisible(false);
+        edit.setVisible(false);
+    }
     private ObservableList<Person> personData = FXCollections.observableArrayList();
     @FXML
     private TableColumn<Person, String> tableFio;
@@ -49,18 +61,38 @@ public class HelloController {
     private TableColumn<Person, String> tablePhone;
     @FXML
     private void initialize() {
-        tableFio.setCellValueFactory(new PropertyValueFactory<Person, String>("phone"));
-        tablePhone.setCellValueFactory(new PropertyValueFactory<Person, String>("fio"));
-
+        unVisible();
+        table.setVisible(true);
+        tableFio.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        tablePhone.setCellValueFactory(new PropertyValueFactory<>("fio"));
     }
     @FXML
     private void add(){
-        personData.add(new Person(value1.getText(),value2.getText()));
+        Person user = new Person(value1.getText(),value2.getText());
+        personData.add(user);
         table.setItems(personData);
         otmena();
+        int j = personData.size();
+        Label text1 = new Label(value1.getText());
+        Label text2 = new Label(value2.getText());
+        Button button = new Button("Delete");
+        gp.add(text1,0,j);
+        gp.add(text2,1,j);
+        button.setId(String.valueOf(personData.size()-1));
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                personData.remove(Integer.parseInt(String.valueOf(button.getId())));
+                gp.getChildren().remove(button);
+                gp.getChildren().remove(text1);
+                gp.getChildren().remove(text2);
+            }
+        });
+        gp.add(button,2,j);
     }
     @FXML
     private void delete(){
-        //personData.remove(1);
+        unVisible();
+        edit.setVisible(true);
     }
 }
