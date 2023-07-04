d064feab94097655d406e7b8111d1c783ee21276
ZHabkaGoti <zhgoti89@gmail.com>
2023-07-04 17:58:22 +0500

    изменения расписания мастеров проверки удаления

--- MFI2/src/main/java/com/example/mfi2/HelloController.java
+++ MFI2/src/main/java/com/example/mfi2/HelloController.java
@@ -15,11 +15,11 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 import java.io.*;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class HelloController {
     private String type = "";
@@ -46,18 +46,22 @@ public class HelloController {
     private GridPane gp;
     @FXML
     private AnchorPane edit;
+
     @FXML
-    protected void clickAdd(){
+    protected void clickAdd() {
         unVisible();
         addForm.setVisible(true);
     }
+
     @FXML
     public void otmena() {
         unVisible();
         table.setVisible(true);
     }
+
     private boolean isLightMode = true;
-  @FXML
+
+    @FXML
     public void switchTheme(ActionEvent event) {
       /*isLightMode = !isLightMode;
         if (isLightMode){
@@ -66,51 +70,61 @@ public class HelloController {
         else {
             setDarkMode();
         }*/
-       if(type.equals("White")){
+        if (type.equals("White")) {
             type = "Red";
             bordertype = "Black";
-        }else{
+        } else {
             type = "White";
             bordertype = "Green";
         }
-       table.setStyle("-fx-background-color:" + type + ";-fx-border-color:" + bordertype);
-       tableFio.setStyle("-fx-background-color:" + type + ";-fx-border-color:" + bordertype);
-       tablePhone.setStyle("-fx-background-color:" + type + ";-fx-border-color:" + bordertype);
-       background.setStyle("-fx-background-color:" + type);
-       sPane.setStyle("-fx-background-color:" + type);
+        table.setStyle("-fx-background-color:" + type + ";-fx-border-color:" + bordertype);
+        tableFio.setStyle("-fx-background-color:" + type + ";-fx-border-color:" + bordertype);
+        tablePhone.setStyle("-fx-background-color:" + type + ";-fx-border-color:" + bordertype);
+        background.setStyle("-fx-background-color:" + type);
+        sPane.setStyle("-fx-background-color:" + type);
     }
-    private void setLightMode(){
+
+    private void setLightMode() {
         sPane.getStylesheets().remove((Objects.requireNonNull(getClass().getResource("styles/DarkMode.css"))).toExternalForm());
         sPane.getStylesheets().add((Objects.requireNonNull(getClass().getResource("styles/LightMode.css"))).toExternalForm());
     }
-    private void setDarkMode(){
+
+    private void setDarkMode() {
         sPane.getStylesheets().remove((Objects.requireNonNull(getClass().getResource("styles/LightMode.css"))).toExternalForm());
         sPane.getStylesheets().add((Objects.requireNonNull(getClass().getResource("styles/DarkMode.css"))).toExternalForm());
     }
-    protected void unVisible(){
+
+    protected void unVisible() {
         table.setVisible(false);
         addForm.setVisible(false);
         edit.setVisible(false);
     }
+
     private ObservableList<Person> personData = FXCollections.observableArrayList();
+
     @FXML
     private void initialize() {
         File file = new File("items.txt");
         try {
-            file.createNewFile();
-            BufferedReader br = new BufferedReader(new FileReader("items.txt"));
-            boolean what = true;
-            while(what){
-                String value1 = br.readLine();
-                if(value1 == null){
-                    what = false;
+            if (file.exists()) {
+                BufferedReader br = new BufferedReader(new FileReader("items.txt"));
+                boolean what = true;
+                while (what) {
+                    String value1 = br.readLine();
+                    if (value1 == null) {
+                        what = false;
+                    } else {
+                        String value2 = br.readLine();
+                        Person user = new Person(value2, value1);
+                        personData.add(user);
+                        table.setItems(personData);
+                    }
                 }
-                String value2 = br.readLine();
-                Person user = new Person(value2, value1);
-                personData.add(user);
-                table.setItems(personData);
+                addDelForm();
+                br.close();
+            } else {
+                file.createNewFile();
             }
-            br.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -120,7 +134,8 @@ public class HelloController {
         tablePhone.setCellValueFactory(new PropertyValueFactory<>("fio"));
         search();
     }
-    private void search(){
+
+    private void search() {
         FilteredList<Person> filteredData = new FilteredList<>(personData, b -> true);
         filterField.textProperty().addListener((observable, oldValue, newValue) -> filteredData.setPredicate(Person -> {
             if (newValue == null || newValue.isEmpty()) {
@@ -139,41 +154,52 @@ public class HelloController {
         sortedData.comparatorProperty().bind(table.comparatorProperty());
         table.setItems(sortedData);
     }
+
     @FXML
     private void add() {
         String a = value2.getText();
         String b = value1.getText();
         try {
-            if (a.matches("\\d{11}") && b.matches("[A-Za-zА-Яа-я]+")) {
+            if (a.matches("8\\d{10}") && b.matches("[A-Za-zА-Яа-я ]+") && !b.equals(" ") && !b.matches(" [A-Za-zА-Яа-я ]+") || !b.matches(" [A-Za-zА-Яа-я ]+") && !b.equals(" ") && a.matches("[+]7\\d{10}") && b.matches("[A-Za-zА-Яа-я]+")) {
                 Person user = new Person(a, b);
                 personData.add(user);
                 table.setItems(personData);
                 otmena();
                 addDelForm();
                 search();
-                PrintWriter pw = new PrintWriter("items.txt");
-                for(int i = 0; i < personData.size(); i++){
-                    pw.println(personData.get(i).getPhone());
-                    pw.println(personData.get(i).getFio());
-                }
-                pw.close();
+                printToFile();
             } else {
                 throw new IllegalArgumentException();
             }
         } catch (IllegalArgumentException e) {
+        }
+    }
+
+    private void printToFile() {
+        try {
+            PrintWriter pw = new PrintWriter("items.txt");
+            for (int i = 0; i < personData.size(); i++) {
+                pw.println(personData.get(i).getPhone());
+                pw.println(personData.get(i).getFio());
+            }
+            pw.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-    private void addDelForm(){
+
+    @FXML
+    private void addDelForm() {
         gp.getChildren().clear();
-        for(int i = 0,d, j = 0; i < personData.size(); i++){
+        for (int i = 0, d, j = 0; i < personData.size(); i++) {
+            gp.getRowConstraints().add(new RowConstraints(30));
+            gp.setGridLinesVisible(true);
             d = 0;
             Label text = new Label(personData.get(i).getFio());
-            gp.add(text,d,j);
+            gp.add(text, d, j);
             d++;
             text = new Label(personData.get(i).getPhone());
-            gp.add(text,d,j);
+            gp.add(text, d, j);
             d++;
             Button button = new Button("Delete");
             button.setId(String.valueOf(j));
@@ -182,26 +208,31 @@ public class HelloController {
                 public void handle(MouseEvent mouseEvent) {
                     personData.remove(Integer.parseInt(String.valueOf(button.getId())));
                     addDelForm();
+                    printToFile();
                 }
             });
-            gp.add(button,d,j);
+            gp.add(button, d, j);
             j++;
         }
     }
+
     @FXML
-    private void delete(){
+    private void delete() {
         unVisible();
         edit.setVisible(true);
     }
+
     Stage stage = new Stage();
+
     @FXML
     private void viewMasters() throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Schedule.fxml")));
         stage.setTitle("Расписание мастеров");
         stage.setScene(new Scene(root));
         stage.show();
-        stage.setX((Stage.getWindows().get(0).getX()+Stage.getWindows().get(0).getWidth())-stage.getWidth());
-        stage.setY(Stage.getWindows().get(0).getY()+30);
+        stage.setX((Stage.getWindows().get(0).getX() + Stage.getWindows().get(0).getWidth()) - stage.getWidth());
+        stage.setY(Stage.getWindows().get(0).getY() + 30);
         Stage.getWindows().get(0).setOnHidden(event -> stage.close());
+        stage.setResizable(false);
     }
 }
